460fb1c5bce532f2b305dd9abd5a07254c49b6a8
Maksim Demianov <maks.demianov@gmail.com>
2020-01-31 12:18:09 +0300

    add swagger to document REST Api

--- src/main/java/com/study/calendarservice/controller/EventController.java
+++ src/main/java/com/study/calendarservice/controller/EventController.java
@@ -3,12 +3,16 @@ package com.study.calendarservice.controller;
 import com.study.calendarservice.model.Event;
 import com.study.calendarservice.model.SearchPeriod;
 import com.study.calendarservice.service.EventService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Api
 @RequestMapping("/users/{userId}")
 public class EventController {
 
@@ -20,33 +24,56 @@ public class EventController {
     }
 
     @GetMapping("/events")
-    public List<Event> getAllEvents(@PathVariable ("userId") long userId){
+    @ApiOperation(value = "View a list of available user events", response = List.class)
+    public List<Event> getAllEvents(@ApiParam(value = "User Id from which events objects will retrieve", required = true)
+                                        @PathVariable ("userId") long userId){
         return eventService.getAllEventsByUserId(userId);
     }
 
     @PostMapping("/events")
-    public Event addEvent(@PathVariable("userId") long userId, @RequestBody Event event){
+    @ApiOperation(value = "Add event")
+    public Event addEvent(@ApiParam(value = "User Id to add user event object", required = true)
+                              @PathVariable("userId") long userId,
+                          @ApiParam(value = "Event object for add. DateTime format: 'HH:mm dd-MM-yyyy'", required = true)
+                          @RequestBody Event event){
         return eventService.addEvent(userId, event);
     }
 
     @GetMapping("/events/{eventId}")
-    public Event getEventById(@PathVariable("userId") long userId, @PathVariable("eventId") long eventId){
+    @ApiOperation(value = "Get event by Id")
+    public Event getEventById(@ApiParam(value = "User Id to get user event object", required = true)
+                                  @PathVariable("userId") long userId,
+                              @ApiParam(value = "Event Id from which event object will retrieve", required = true)
+                              @PathVariable("eventId") long eventId){
         return eventService.getEventById(userId, eventId);
     }
 
     @PutMapping("/events/{eventId}")
-    public Event editEventById(@PathVariable("userId") long userId, @PathVariable("eventId") long eventId,
+    @ApiOperation(value = "Edit event")
+    public Event editEventById(@ApiParam(value = "User Id to update user event object", required = true)
+                                @PathVariable("userId") long userId,
+                               @ApiParam(value = "Event Id from which event object will edit", required = true)
+                               @PathVariable("eventId") long eventId,
+                               @ApiParam(value = "Edit event object. DateTime format: 'HH:mm dd-MM-yyyy'", required = true)
                                @RequestBody Event event){
         return eventService.editEvent(userId, eventId, event);
     }
 
     @DeleteMapping("/events/{eventId}")
-    public void deleteEvent(@PathVariable("userId") long userId, @PathVariable("eventId") long eventId){
+    @ApiOperation(value = "Delete event")
+    public void deleteEvent(@ApiParam(value = "User Id to delete user event object", required = true)
+                                @PathVariable("userId") long userId,
+                            @ApiParam(value = "Event Id from which event object will delete", required = true)
+                            @PathVariable("eventId") long eventId){
         eventService.deleteEvent(userId, eventId);
     }
 
     @PostMapping("/search")
-    public List<Event> getEventsOverCurrentPeriod(@PathVariable("userId") long userId, @RequestBody SearchPeriod search){
+    @ApiOperation(value = "Search events over current datetime period")
+    public List<Event> getEventsOverCurrentPeriod(@ApiParam(value = "User Id to find user event objects")
+                                                   @PathVariable("userId") long userId,
+                                        @ApiParam(value = "Search period object. DateTime format: 'HH:mm dd-MM-yyyy'")
+                                        @RequestBody SearchPeriod search){
         return eventService.getEventsOverCurrentPeriod(userId, search);
     }
 }
--- src/main/java/com/study/calendarservice/controller/UserController.java
+++ src/main/java/com/study/calendarservice/controller/UserController.java
@@ -2,12 +2,16 @@ package com.study.calendarservice.controller;
 
 import com.study.calendarservice.model.User;
 import com.study.calendarservice.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Api
 public class UserController {
 
     private UserService userService;
@@ -18,32 +22,42 @@ public class UserController {
     }
 
     @GetMapping("/")
+    @ApiOperation(value = "Home page")
     public String welcome(){
-        return "Welcome";
+        return "Welcome to Homepage REST web App";
     }
 
     @GetMapping("/users")
+    @ApiOperation(value = "View a list of available users", response = List.class)
     public List<User> getAllUsers(){
         return userService.getAllUsers();
     }
 
-    @GetMapping("/users/{userId}")
-    public User getUserById(@PathVariable("userId") long userId){
-        return userService.getUserById(userId);
-    }
-
     @PostMapping("/users")
-    public User addUser(@RequestBody User user){
+    @ApiOperation(value = "Add user")
+    public User addUser(@ApiParam(value = "User object for add", required = true)@RequestBody User user){
         return userService.addUser(user);
     }
 
+    @GetMapping("/users/{userId}")
+    @ApiOperation(value = "Get user by Id")
+    public User getUserById(@ApiParam(value = "User Id from which user object will retrieve", required = true)
+                                @PathVariable("userId") long userId){
+        return userService.getUserById(userId);
+    }
+
     @PutMapping("/users/{userId}")
-    public User editUser(@PathVariable("userId") long userId, @RequestBody User user){
+    @ApiOperation(value = "Edit user")
+    public User editUser(@ApiParam(value = "User Id from which user object will edit", required = true)
+                             @PathVariable("userId") long userId,
+                         @ApiParam(value = "Edit user object", required = true)@RequestBody User user){
         return userService.editUser(userId, user);
     }
 
     @DeleteMapping("/users/{userId}")
-    public void deleteUser(@PathVariable("userId") long userId){
+    @ApiOperation(value = "Delete user")
+    public void deleteUser(@ApiParam(value = "User Id from which user object will delete", required = true)
+                               @PathVariable("userId") long userId){
         userService.deleteUser(userId);
     }
 }
